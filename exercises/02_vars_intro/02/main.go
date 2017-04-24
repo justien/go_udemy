e55877d21e07855e246cd9cd15ac2b5cd8501aff
Justine Lera <justinelera@gmail.com>
2017-04-24 18:46:46 +0200

    adding new folder

---
+++ exercises/02_vars_intro/02/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+	e := "Hello"
+	f := `Do you like our owl?`
+	g := 'M'
+
+	fmt.Printf(" %v is %T \n", a, a)
+	fmt.Printf(" \"%v\" is %T \n", b, b)
+	fmt.Printf(" %v is %T \n", c, c)
+	fmt.Printf(" %v is %T \n", d, d)
+	fmt.Printf(" \"%v\" is %T \n", e, e)
+	fmt.Printf(" %v is %T \n", f, f)
+	fmt.Printf(" %v is %T \n", g, g)
+}
