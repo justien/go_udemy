40a470b19fc014e52ecc6357b8eacfd78d961dd0
Justine Lera <justinelera@gmail.com>
2017-05-04 16:17:43 +0200

    updated loop scripts

--- exercises/10_forLoop/02_nested/main.go
+++ exercises/10_forLoop/02_nested/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 15; i++ {
+	for i := 1; i <= 5; i++ {
 		fmt.Println(i)
 		for i := 1; i <= 3; i++ {
 			fmt.Println("  ", i)
--- exercises/10_forLoop/03_forCondsWhileIsh/main.go
+++ exercises/10_forLoop/03_forCondsWhileIsh/main.go
@@ -11,13 +11,32 @@ func random(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
+func Ordinals(i int) string {
+
+	if i == 1 {
+		return "st"
+	} else if i == 2 {
+		return "nd"
+	} else if i == 3 {
+		return "rd"
+	} else {
+		return "th"
+	}
+}
+
 func main() {
 	i := 1
 	for i < 7 {
 		fmt.Println()
-		fmt.Printf("i is %d\n", i)
-		thisrand := random(i, i*100)
-		fmt.Println("this gem shines like the forgotten number ", thisrand)
+		ordinalsuffix := Ordinals(i)
+		fmt.Printf("the %d%s", i, ordinalsuffix)
+		fmt.Printf(" gem shines like the abandoned number ")
+		thisrandmin := random(i, i*100)
+		// fmt.Println(thisrandmin)
+		thisrandmax := random(i*100, i*1000)
+		// fmt.Println(thisrandmax)
+		thisrand := random(thisrandmin, thisrandmax)
+		fmt.Println(thisrand)
 		i++
 	}
 
