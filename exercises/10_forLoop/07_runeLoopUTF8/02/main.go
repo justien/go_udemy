98ff1b3bcdc902a3ceea3a4e745635b1a0e9d409
Justine Lera <justinelera@gmail.com>
2017-05-05 14:12:10 +0200

    added more interest to rune loop

--- exercises/10_forLoop/07_runeLoopUTF8/02/main.go
+++ exercises/10_forLoop/07_runeLoopUTF8/02/main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	for i := 120; i <= 140; i++ {
-		fmt.Printf("%v - %v, %v \n", i, string(i), []byte(string(i)))
+	fmt.Println([]byte("Hello"))
+	for i := 5000; i <= 5010; i++ {
+		fmt.Println(i, " - ", string(i), " - ", []byte(string(i)))
+	}
+	for i := 18765; i <= 18775; i++ {
+		fmt.Printf("%v - %v - %v\n", i, string(i), []byte(string(i)))
 	}
 }
