520ff15b7520a79cb43a38d52ec07b01d2732506
Justine Lera <justinelera@gmail.com>
2017-05-12 11:32:48 +0200

    added script for sum of naturals

--- exercises/13_exercises/07_NaturalSum/main.go
+++ exercises/13_exercises/07_NaturalSum/main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 
+	running_total := 0
+
+	for i := 0; i < 100; i++ {
+
+		if i%3 == 0 || i%5 == 0 {
+			fmt.Println("i is a hit with value", i)
+			hit := i
+			running_total = running_total + hit
+			fmt.Println("Current total of all hits is", running_total)
+		}
+	}
+
 }
