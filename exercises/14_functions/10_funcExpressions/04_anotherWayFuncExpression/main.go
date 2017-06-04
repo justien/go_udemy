3e34e9ba8ee1e7ac7a2ba8eac76474114728cd38
Justine Lera <justinelera@gmail.com>
2017-06-04 13:52:28 +0200

    added mysteriious script to functions topic

---
+++ exercises/14_functions/10_funcExpressions/04_anotherWayFuncExpression/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func makeGreeter() func() string {
+
+	x := 4
+	fmt.Println("\n")
+	fmt.Println("MG: makeGreeter() begins to exist.")
+	fmt.Println("MG: I emit the value of a string-returning function.")
+	fmt.Println("MG: I have a variable x, with value ", x)
+	fmt.Println("\n")
+
+	return func() string {
+
+		n := "Hello world"
+		fmt.Println("\n")
+		fmt.Println("AF: anonymous function func() begins to exist.")
+		fmt.Printf("AF: I emit a %T with value %s", n, n)
+		fmt.Println(n)
+		fmt.Println("\n")
+		return n
+	}
+}
+
+func main() {
+
+	fmt.Println("\n")
+	fmt.Println("MAIN: main() begins to exist.\n")
+	greet := makeGreeter()
+	fmt.Println("MAIN: I have a variable greet, which is a value with type %T", greet)
+	fmt.Println("MAIN: Now we print the value of greet():", greet())
+}
+
+/* NOTE WELL:
+   as soon as a func expression is declared, any non-returned
+   instructions are executed!
+   A named function
+*/
