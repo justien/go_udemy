1c5102613403f9adfe58aae0c1776120409bed75
Justine Lera <justinelera@gmail.com>
2017-06-05 10:07:53 +0200

    added scripts to closure topic

--- exercises/14_functions/11_closure/03/main.go
+++ exercises/14_functions/11_closure/03/main.go
@@ -3,11 +3,20 @@ package main
 import "fmt"
 
 func main() {
+
 	x := 0
+
 	increment := func() int {
 		x++
 		return x
 	}
+
 	fmt.Println("The 1st increment of x:", increment())
 	fmt.Println("The 2nd increment of x:", increment())
 }
+
+/*
+closure helps us limit the scope of variables used by multiple functions.
+in the above example, x is only available to two functions bc one
+has been anonymously embedded inside main()
+*/
