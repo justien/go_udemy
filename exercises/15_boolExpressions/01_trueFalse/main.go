f8faf9d9ee578ce725f7a26970223bb12f788a5b
Justine Lera <justinelera@gmail.com>
2017-06-13 17:18:31 +0200

    added folders for boolean topic

---
+++ exercises/15_boolExpressions/01_trueFalse/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if true {
+		fmt.Println("It is true, so I got printed")
+	}
+
+	if false {
+		fmt.Println("Only if it evals to false will you see this")
+	}
+}
