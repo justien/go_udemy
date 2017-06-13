f4db057db1cc9773650077e07c061819013cd9af
Justine Lera <justinelera@gmail.com>
2017-06-13 16:00:54 +0200

    added folders and scripts for pass by value in functions topic

--- exercises/14_functions/15_passingByValue/01_int/main.go
+++ exercises/14_functions/15_passingByValue/01_int/main.go
@@ -2,37 +2,16 @@ package main
 
 import "fmt"
 
-func changeMe(z *int) {
-
-	fmt.Println("\n\n")
-
-	fmt.Println("changeMe() has been called into being")
-
-	fmt.Println("z has the value", z)
-	fmt.Println("z has the memory location", *z)
-
-	*z = 24
-
-	fmt.Println("z now has the stored value", z)
-	fmt.Println("z has the memory location", *z)
-
-	fmt.Println("\n\n")
-}
-
 func main() {
-
 	age := 44
-	fmt.Println("age has the memory location", &age)
-
-	changeMe(&age)
-
-	fmt.Println("age has the memory location", &age)
-	fmt.Println("age has the stored value", age)
+	changeMe(age)
+	fmt.Println(age) // 44
 }
 
-/*
-PASSING BY VALUE
+func changeMe(z int) {
+	fmt.Println(z)
+	z = 24
+}
 
-Once age is declared (a := 44), we now have a memory location with a value.
-
-*/
+// when changeMe is called on line 8
+// the value 44 is being passed as an argument
