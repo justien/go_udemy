b902de36ae6926db9241c4961fda099881d393a7
Justine Lera <justinelera@gmail.com>
2017-06-22 19:08:32 +0200

    added scripts to slices topic

--- exercises/18_slices/12_multi-dimensional/01_shorthand-slice/main.go
+++ exercises/18_slices/12_multi-dimensional/01_shorthand-slice/main.go
@@ -7,6 +7,10 @@ func main() {
 	student := []string{}
 	students := [][]string{}
 
+	// student[0] = "Todd"
+
+	student = append(student, "Todd")
+
 	fmt.Println("Here is student", student)
 	fmt.Println("Here is students", students)
 
--- exercises/18_slices/12_multi-dimensional/02_var-slice/main.go
+++ exercises/18_slices/12_multi-dimensional/02_var-slice/main.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main() {
+
 	var student []string
-	var students []string
+	var students [][]string
+
+	// student[0] = "Todd"
+
+	student = append(student, "Todd")
+
 	fmt.Println("Here is students", students)
 	fmt.Println("Here is student", student)
+
 	fmt.Println("Is the slice student empty?", student == nil)
 }
