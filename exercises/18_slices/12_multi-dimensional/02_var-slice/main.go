cce7747d65e13beaa53f963a5909f866b5e104c0
Justine Lera <justinelera@gmail.com>
2017-06-22 19:00:07 +0200

    added scripts to slices topic

---
+++ exercises/18_slices/12_multi-dimensional/02_var-slice/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var student []string
+	var students []string
+	fmt.Println("Here is students", students)
+	fmt.Println("Here is student", student)
+	fmt.Println("Is the slice student empty?", student == nil)
+}
