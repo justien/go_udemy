609f6fef0554baff6c769e77c19aa417dddd0f76
Justine Lera <justinelera@gmail.com>
2017-06-23 18:31:51 +0200

    added script to maps topic

--- exercises/19_map/01_initialisingMaps/main.go
+++ exercises/19_map/01_initialisingMaps/main.go
@@ -9,11 +9,8 @@ func main() {
 	fmt.Println(myGreeting)
 	fmt.Println(myGreeting == nil)
 
-	/* add these lines:
-
-	   	myGreeting["Tim"] = "Good morning."
+	/*
 	   	myGreeting["Jenny"] = "Bonjour."
-
 	    and you will get this:
 	    panic: assignment to entry in nil map
 	*/
