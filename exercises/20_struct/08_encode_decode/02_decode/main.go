e3f97f2465535e5c7492e068934927bf84616234
Justine Lera <justinelera@gmail.com>
2017-06-27 15:11:23 +0200

    added scripts to struct topic

--- exercises/20_struct/08_encode_decode/02_decode/main.go
+++ exercises/20_struct/08_encode_decode/02_decode/main.go
@@ -14,8 +14,15 @@ type person struct {
 }
 
 func main() {
+
+	// Gidday var, make me an empty p1 of type person thx!
 	var p1 person
+
+	// Gday Strings, NewReader the following slice of bytes thx!
 	rdr := strings.NewReader(`{"First":"James", "Last":"Bond", "Age":20}`)
+
+	// Heya Jason, NewDecode from/using rdr into the address of p1
+	// in its Decode (receiving) manifestation thx!
 	json.NewDecoder(rdr).Decode(&p1)
 
 	fmt.Println(p1.First)
