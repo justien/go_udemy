1d8173ad9fb0590d834e33df41e811a8cc708bef
Justine Lera <justinelera@gmail.com>
2017-06-30 10:44:37 +0200

    edited scripts in interfaces topic

--- exercises/21_interfaces/01_interface/03_interface_polymorphic/main.go
+++ exercises/21_interfaces/01_interface/03_interface_polymorphic/main.go
@@ -9,7 +9,6 @@ type square struct {
 	side float64
 }
 
-// another shape
 type circle struct {
 	radius float64
 }
--- exercises/21_interfaces/01_interface/06_dog_Implements_Tail/01_multi-value_struct/main.go
+++ exercises/21_interfaces/01_interface/06_dog_Implements_Tail/01_multi-value_struct/main.go
@@ -5,9 +5,10 @@ import (
 	"math"
 )
 
+// Structs -------------------------------------
 type square struct {
-	side float64
-	//colour string
+	side   float64
+	colour string
 }
 
 type circle struct {
@@ -15,10 +16,16 @@ type circle struct {
 	emotion string
 }
 
-// Dimension interface and methods --------------
+// Interfaces --------------
 type dimension interface {
 	area() float64
 }
+
+type shadow interface {
+	tell_me() string
+}
+
+// Methods ------------------
 
 func (s square) area() float64 {
 	return s.side * s.side
@@ -28,36 +35,39 @@ func (c circle) area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
-//shadow interface and methods ------------------
-type shadow interface {
-	tell_me() string
-}
-
 // func (s square) tell_me() string {
 // 	w := s.colour
 // 	return w
 // }
 
+func (s circle) tell_me() string {
+	w := fmt.Sprintf("%v", s.emotion)
+	return w
+}
+
 type myTest interface {
 	tell_me() string
 }
 
-func (s circle) tell_me() string {
-	w := s.emotion
-	return w
-}
-
 // functions ------------------------------------
 func info(a dimension) {
-	fmt.Println(a)
+	fmt.Println("The area is:", a.area())
 }
+
+// I don't get why I can't access circle.emotion's value
 
 func the_cloud(a shadow) {
-	fmt.Println("You step out into the shadow of a", a, "cloud")
+	c := a.tell_me
+	fmt.Println("Here is c", c)
+	fmt.Printf("Here is value of c %v", c)
+
+	//fmt.Println(fmt.Sprintf("You step out into the shadow of a %v cloud \n", a.tell_me())
+	fmt.Println()
 }
 
+// the headlining act ---------------------------
 func main() {
-	s := square{10}
+	s := square{10, "red"}
 	c := circle{5, "recriminative"}
 	info(s)
 	the_cloud(c)
